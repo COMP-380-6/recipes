29e157d359bbc189a06d3198c1ef7c84181b95ec
1101011 <85262772+1101011@users.noreply.github.com>
2021-06-12 13:01:38 -0700

    Load environment variables from a .env file
    Do not install the dotenv dependency or load the .env file in
    production. Those should either be in config.py or defined in AWS.

--- web/__init__.py
+++ web/__init__.py
@@ -2,6 +2,14 @@ from flask import Flask
 from flask_cdn import CDN
 from whitenoise import WhiteNoise
 
+# dotenv is not present (nor does it need to be) in production.
+try:
+    import dotenv
+
+    dotenv.load_dotenv()
+except ImportError:
+    pass
+
 cdn = CDN()
 
 
