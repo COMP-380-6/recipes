d3c3ae72d277c83e46d7f393501a29448a50d413
1101011 <85262772+1101011@users.noreply.github.com>
2021-06-25 13:44:54 -0700

    Frontend: fix equipment being duplicated

--- web/static/modal.js
+++ web/static/modal.js
@@ -118,10 +118,15 @@ export class RecipeModal extends PaginatedModal {
         const template = this._element.querySelector("#req-equip-template");
         template.parentElement.replaceChildren(template);
 
+        const seen = new Set(); // Used to skip duplicate equipment.
+
         for (const instructions of recipe.analyzedInstructions) {
             for (const step of instructions.steps) {
                 for (const equip of step.equipment) {
-                    this._createRequirement(equip, template, "equipment");
+                    if (!seen.has(equip.id)) {
+                        this._createRequirement(equip, template, "equipment");
+                        seen.add(equip.id);
+                    }
                 }
             }
         }
