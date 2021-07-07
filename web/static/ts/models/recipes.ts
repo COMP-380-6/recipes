c7984f5e860eeff21e06cfec9a9650ca9494e4a0
1101011 <85262772+1101011@users.noreply.github.com>
2021-07-06 17:44:03 -0700

    Remove autocomplete options field in view
    Resolve #28

--- web/static/ts/models/recipes.ts
+++ web/static/ts/models/recipes.ts
@@ -1,10 +1,12 @@
 import {Client} from "../api";
 import {Recipe, RecipeSearchResults} from "./spoonacular";
 import {APIObservable} from "../observe";
+import isequal from "lodash.isequal";
 
 export class Recipes extends APIObservable<Recipe[]> {
     protected readonly _api: Client;
     private _data: Recipe[] = [];
+    private _prevParams: Record<string, any> = {};
 
     constructor(apiClient: Client) {
         super();
@@ -37,10 +39,16 @@ export class Recipes extends APIObservable<Recipe[]> {
             maxReadyTime: maxReadyTime,
         };
 
+        // Avoid redundant requests.
+        if (isequal(params, this._prevParams)) {
+            return;
+        }
+
         try {
             const response = await this._api.get("search", params);
             const results = (await response.json()) as RecipeSearchResults;
             this.data = results.results;
+            this._prevParams = params;
         } catch (e) {
             this._notifyResponseError(e);
         }
