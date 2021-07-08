ff0b52c00ec1e2124f13b3d7786ec27251d2ae42
1101011 <85262772+1101011@users.noreply.github.com>
2021-07-08 10:29:04 -0700

    Webpack Font Awesome's CSS
    Fix the CSS not being loaded due to the CSP rejecting inline styles.

--- web/static/ts/fontawesome.ts
+++ web/static/ts/fontawesome.ts
@@ -1,5 +1,9 @@
-import {library, dom} from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import {library, dom, config} from "@fortawesome/fontawesome-svg-core";
 import {fas} from "@fortawesome/free-solid-svg-icons";
+
+// Include CSS in webpack instead to avoid having to allow inline styles in CSP.
+config.autoAddCss = false;
 
 library.add(fas);
 dom.watch();
